c48c8980de107696b92455e425d3574b1bda602b
MarceloYevenes <yevenesmarcelo1@gmail.com>
2019-07-13 16:14:06 -0400

    Archivo conexión actualizado

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,1 +1,13 @@
+<?php 
+		function conexion(){
+			$servidor="146.83.194.142";
+			$usuario="grupo4";
+			$password="grupo4optica";
+			$bd="grupo4bd";
 
+			$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
+
+			return $conexion;
+		}
+
+ ?>
