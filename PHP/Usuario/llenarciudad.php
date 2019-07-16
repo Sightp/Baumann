dc9c07a91220ff22a6eacd60d761ed75f6aa89ed
MarceloYevenes <yevenesmarcelo1@gmail.com>
2019-07-15 20:03:27 -0400

    actualizado php usuario registro

--- PHP/Usuario/llenarciudad.php
+++ PHP/Usuario/llenarciudad.php
@@ -3,13 +3,12 @@
     require_once "../conexion.php";
     $conexion=conexion();
     
-    $sql = "SELECT REG_ID, REG_NOMBRE FROM REGION";
+    $region = $_POST['region'];
+    $sql = "SELECT CIUDAD.CIU_ID, CIUDAD.CIU_NOMBRE FROM CIUDAD WHERE CIUDAD.REG_ID='$region'";
     $result=mysqli_query($conexion,$sql);
    
     echo '<option selected value="0">--Seleccione--</option>'; 
     while ($fila = mysqli_fetch_array($result)) {
-        echo '<option value="'.$fila['REG_ID'].'">'.$fila['REG_NOMBRE'].'</option>';
+        echo '<option value="'.$fila['CIU_ID'].'">'.$fila['CIU_NOMBRE'].'</option>';
     };
     
-?>
- 
--- PHP/Usuario/registro.php
+++ PHP/Usuario/registro.php
@@ -1,14 +1,28 @@
 <?php 
 
-	require_once "conexion.php";
+	require_once "../conexion.php";
 	$conexion=conexion();
-	$n=$_POST['nombre'];
-	$a=$_POST['apellido'];
-	$e=$_POST['email'];
-	$t=$_POST['telefono'];
 
-	$sql="INSERT into t_persona (nombre,apellido,email,telefono)
-								values ('$n','$a','$e','$t')";
+	$rut=$_POST['rut'];
+	$nombre=$_POST['nombre'];
+	$apellidoP=$_POST['apellidoP'];
+	$apellidoM=$_POST['apellidoM'];
+	$pass=$_POST['pass'];
+	$direccion=$_POST['direccion'];
+	$telefono=$_POST['telefono'];
+	$correo=$_POST['correo'];
+	$ciudad=$_POST['ciudad'];
+
+
+	if(empty($ciudad)){
+		$sql="INSERT INTO CLIENTE(CLI_RUT, CLI_NOMBRE, CLI_PATERNO, CLI_MATERNO, CLI_PASS, CLI_DIRECCION, CLI_TELEFONO, CLI_CORREO) 
+					  VALUES ('$rut','$nombre','$apellidoP','$apellidoM','$pass','$direccion','$telefono','$correo')";
+	}else{
+		$sql="INSERT INTO CLIENTE(CLI_RUT, CLI_NOMBRE, CLI_PATERNO, CLI_MATERNO, CLI_PASS, CLI_DIRECCION, CLI_TELEFONO, CLI_CORREO, CIU_ID) 
+					  VALUES ('$rut','$nombre','$apellidoP','$apellidoM','$pass','$direccion','$telefono','$correo','$ciudad')";
+	}
+	
+
 	echo $result=mysqli_query($conexion,$sql);
 
  ?>
