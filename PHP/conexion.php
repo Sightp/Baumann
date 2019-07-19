c04b781877665fccb9b00b4b49824d64044064b0
MarceloYevenes <44509303+MarceloYevenes@users.noreply.github.com>
2019-07-19 17:21:43 -0400

    Add files via upload

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -4,8 +4,9 @@
 			$usuario="root";
 			$password="";
 			$bd="grupo4bd";
-
+		  
 			$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
+			$con=mysqli_set_charset($conexion,'utf8');
 
 			return $conexion;
 		}
