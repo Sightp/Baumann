c5351ab926dc5c05d73412bb3b7957fad4cfef5e
SSentry <45566153+SSentry@users.noreply.github.com>
2019-07-20 11:28:42 -0600

    Add files via upload

--- PHP/proveedor/tabla.php
+++ PHP/proveedor/tabla.php
@@ -5,12 +5,13 @@
 
 ?>
 
-<table class="table table-hover table-condensed table-bordered">
+<table class="table table-hover table-condensed table-bordered" id="tablaproveedor">
     <caption>
         <button class="btn btn-primary" data-toggle="modal" data-target="#modalNuevo">Agregar
             <span class="glyphicon glyphicon-plus"><span>
         </button>
     </caption> 
+    <thead>
     <tr>  
         <td>Rut</td>
         <td>Nombre </td>
@@ -19,6 +20,8 @@
         <td>Editar</td>
         <td>Eliminar</td>
     </tr>
+    </thead>
+    <tbody>
 
     <?php
         
@@ -64,5 +67,42 @@
     <?php   
         }//FIN WHILE//
     ?>
-    <tbody id="tasks"></tbody>
+    	</tbody>
 </table>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+	$('#tablaproveedor').DataTable({
+		language :
+
+		{
+			"sProcessing":     "Procesando...",
+			"sLengthMenu":     "Mostrar _MENU_ registros",
+			"sZeroRecords":    "No se encontraron resultados",
+			"sEmptyTable":     "Ningún dato disponible en esta tabla",
+			"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+			"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+			"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+			"sInfoPostFix":    "",
+			"sSearch":         "Buscar:",
+			"sUrl":            "",
+			"sInfoThousands":  ",",
+			"sLoadingRecords": "Cargando...",
+			"oPaginate": {
+				"sFirst":    "Primero",
+				"sLast":     "Último",
+				"sNext":     "Siguiente",
+				"sPrevious": "Anterior"
+			},
+			"oAria": {
+				"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+				"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+			}
+		}
+
+		
+	});
+});
+
+</script>
