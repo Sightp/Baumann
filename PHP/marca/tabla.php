bd8e1e6277e9fda4ef1ffc200fd52cb027398886
SSentry <45566153+SSentry@users.noreply.github.com>
2019-07-20 11:28:08 -0600

    Add files via upload

--- PHP/marca/tabla.php
+++ PHP/marca/tabla.php
@@ -8,19 +8,25 @@
 <div class="row">
 	<div class="col-sm-12">
 	<h2>Administrador de Marca de un Producto</h2>
-		<table class="table table-hover table-condensed table-bordered">
+		<table class="table table-hover table-condensed table-bordered" id="tabladinamicamarca">
 		<caption>
 			<button class="btn btn-primary" data-toggle="modal" data-target="#modalNuevo">
 				Agregar nuevo 
 				<span class="glyphicon glyphicon-plus"></span>
 			</button>
 		</caption>
+
+		<thead>
+
 			<tr>
 				<td>CODIGO</td>
 				<td>NOMBRE</td>
 				<td>Editar</td>
 				<td>Eliminar</td>
 			</tr>
+
+		</thead>
+		<tbody>
 
 			<?php 
 
@@ -62,6 +68,44 @@
 			<?php 
 		}
 			 ?>
+			 		</tbody>
+
 		</table>
 	</div>
 </div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	$('#tabladinamicamarca').DataTable({
+		language :
+
+		{
+			"sProcessing":     "Procesando...",
+			"sLengthMenu":     "Mostrar _MENU_ registros",
+			"sZeroRecords":    "No se encontraron resultados",
+			"sEmptyTable":     "Ningún dato disponible en esta tabla",
+			"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+			"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+			"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+			"sInfoPostFix":    "",
+			"sSearch":         "Buscar:",
+			"sUrl":            "",
+			"sInfoThousands":  ",",
+			"sLoadingRecords": "Cargando...",
+			"oPaginate": {
+				"sFirst":    "Primero",
+				"sLast":     "Último",
+				"sNext":     "Siguiente",
+				"sPrevious": "Anterior"
+			},
+			"oAria": {
+				"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+				"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+			}
+		}
+
+		
+	});
+});
+
+</script>
