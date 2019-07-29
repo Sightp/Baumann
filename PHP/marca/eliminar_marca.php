fa64a3be472a32876a5b87f578e5f6295002d12f
SSentry <45566153+SSentry@users.noreply.github.com>
2019-07-29 13:57:56 -0600

    Delete eliminar_marca.php

--- PHP/marca/eliminar_marca.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-	require_once "../conexion.php";
-	$conexion=conexion();
-	$codigo=$_POST['codigo'];
-
-	$sql="DELETE FROM MARCA_PRODUCTO where MAR_ID='$codigo'";
-	echo $result=mysqli_query($conexion,$sql);
- ?>
