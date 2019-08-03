886b24291d744b427492082b6e553fa5f6647253
pereira23 <49495870+pereira23@users.noreply.github.com>
2019-08-03 16:24:50 -0400

    Delete eliminardatos.php

--- PHP/Trabajador/inventario/eliminardatos.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require_once"../conexion.php";
-  $conexion= conexion();
-  
-  $id=$_POST['proid'];
-  $sql="DELETE FROM producto WHERE PRO_ID='$id'";
-  echo $result=mysqli_query($conexion,$sql);
-?>
